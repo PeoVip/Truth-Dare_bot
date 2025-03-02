5bc18a08a037cec01898c53ae4a16576c5a9fd9e
PeoVip <levanpeo1213@gmail.com>
2025-03-02 15:01:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from discord.ext import commands
 from keep_alive import keep_alive 
 
+Token='MTM0NTY2NTE1ODQyMDEwNzMwNg.Gi9eJM.Wdx3RWRKfbD9tlZntmK-_ovndZYNpeWUJIQY5Y'
 intents = discord.Intents.default()
 intents.message_content = True 
 
